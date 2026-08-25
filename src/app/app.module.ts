07ad316df02705bdfe3596628e2a6782cdc9641f
Alexander Aracena <alexander.aracena.90@gmail.com>
2026-08-24 23:54:43 -0300

    Practica de Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddComponent } from './components/addTask/add.component';
 import { ListTaskComponent } from './components/list-task/list-task.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, AddComponent, ListTaskComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent]
 })
--- src/app/components/addTask/add.component.ts
+++ src/app/components/addTask/add.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-addTask',
@@ -7,12 +7,17 @@ import { AfterContentInit, Component, OnDestroy, OnInit } from '@angular/core';
   styleUrl: './add.component.css'
 })
 
-export class AddComponent implements OnDestroy, AfterContentInit {
-  ngOnDestroy(): void {
-    console.log('El componente AddComponent ha sido destruido');
-  }
+export class AddComponent {
+  numberTasks: number = 10;
+  titleTask: string = '';
+  activeButton: boolean = true;
 
-  ngAfterContentInit(): void {
-    console.log('El contenido proyectado ha sido inicializado');
+  sendTask() {
+    const sizeTitleTask = this.titleTask.split('');
+    if (sizeTitleTask.length > 0) {
+      this.activeButton = false;
+    } else {
+      this.activeButton = true;
+    }
   }
 }
